00184414d9d95ed3e344c9d01b049518fed853a2
Davronbek <drahmonov@mail.ru>
2018-01-12 13:56:38 +0500

    loop added

--- python_script.py
+++ python_script.py
@@ -1,10 +1,23 @@
 import requests
 
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+    sleep(3)       
+
+if __name__ == '__main__':  
+    main()
+
+
 url = "https://api.telegram.org/bot514193432:AAHI910wcfgiRPiD8f1NyiOFkJ5I_-of870/"
 
 
 def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
     return response.json()
 
 
